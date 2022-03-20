5ccc2237201cd96079823bacc5200cf873b0c719
Tsebo <tsebo@lumkani.com>
2022-03-20 10:48:10 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,74 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+
+/** google_map js **/
+function myMap() {
+    var myLatlng = new google.maps.LatLng(-26.260611, 28.255501);
+    var mapProp = {
+        center: myLatlng,
+	       	zoom:18,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			draggable: true,
+			scrollwheel: false,
+			zoomControl: true,
+			zoomControlOptions:
+			{
+				position: google.maps.ControlPosition.RIGHT_CENTER
+			},
+			mapTypeControl: false,
+			scaleControl: false,
+			streetViewControl: false,
+			rotateControl: false,
+			fullscreenControl: true,
+			styles:
+			[
+			  {
+			    "featureType": "road.highway",
+			    "elementType": "geometry.fill",
+			    "stylers": [
+			      {
+			        "color": "#ffeba1"
+			      }
+			    ]
+			  }
+			]
+        
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    return map
+}
